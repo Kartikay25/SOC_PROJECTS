917cab1b04041a61b1b9e7eb024dbf184cc50ced
Kartikay Raina <karmatiit123456@gmail.com>
2026-06-24 10:41:20 +0530

    Create player.py

---
+++ WEEK_2/player.py
@@ -0,0 +1,24 @@
+class Player:
+    def __init__(self, start_row, start_col, start_direction, color, player_id):
+      
+        self.start_row       = start_row
+        self.start_col       = start_col
+        self.start_direction = start_direction
+ 
+        # current state
+        self.row       = start_row
+        self.col       = start_col
+        self.direction = start_direction   
+        self.color     = color
+        self.player_id = player_id
+        self.alive     = True
+ 
+        
+        self.trail = []
+ 
+    def reset(self):
+        self.row       = self.start_row
+        self.col       = self.start_col
+        self.direction = self.start_direction
+        self.alive     = True
+        self.trail     = []
