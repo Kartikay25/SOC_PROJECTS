0ac197297a53ed8ca9c604e5679c4effa5627aa9
Kartikay Raina <karmatiit123456@gmail.com>
2026-06-24 10:49:44 +0530

    Create constants.py

---
+++ Week_3/constants.py
@@ -0,0 +1,16 @@
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 800
+ROWS      = 80
+COLS      = 80
+CELL_SIZE = SCREEN_WIDTH // COLS   
+
+FPS = 10
+ 
+BLACK     = (0,   0,   0)
+WHITE     = (255, 255, 255)
+GRAY      = (60,  60,  60)
+ 
+RED       = (220, 50,  50)
+DARK_RED  = (110, 20,  20)   
+BLUE      = (50,  100, 220)
+DARK_BLUE = (20,  30,  120) 
